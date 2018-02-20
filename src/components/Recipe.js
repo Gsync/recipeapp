e6ce3da057dbe725a347da572bebf4d1f77fa2d2
Khuram Niaz <knshaikh@gmail.com>
2018-02-20 14:57:20 -0700

    recipe props

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 class Recipe extends Component {
     render() {
+        const title = this.props.title; //same as const {title} = this.props;
+        const ingredients = this.props.ingredients
+            .map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+            ));
         return (
-            <div>Recipe</div>
+            <div>
+                <div>Recipe {title} </div>
+                <ul>{ingredients}</ul>
+            </div>
         );
     }
 }
