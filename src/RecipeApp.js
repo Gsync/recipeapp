ab6b35f228192d3ec1363284000af5be20292da7
Khuram Niaz <knshaikh@gmail.com>
2018-02-20 19:50:39 -0700

    refactor to use state

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -6,11 +6,40 @@ import RecipeList from './components/RecipeList';
 
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: "Pasta",
+          instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
+          ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
+          image: "images/spaghetti.jpg",
+        },
+        {
+          id: 2,
+          title: "Banana Cake",
+          instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
+          ingredients: ["Bananas", "3 cups of water", "Half cup of oil"],
+          image: "images/spaghetti.jpg",
+        },
+        {
+          id: 3,
+          title: "Biryani",
+          instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
+          ingredients: ["Rice", "3 cups of water", "Half cup of oil"],
+          image: "images/spaghetti.jpg",
+        }
+      ],
+      nextRecipeId: 3,
+    };
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes} />
       </div>
     );
   }
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,53 +4,13 @@ import Recipe from './Recipe';
 import './RecipeList.css';
 
 class RecipeList extends Component {
-    static defaultProps = {
-        recipes: [
-            {
-                title: "Pasta",
-                instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
-                ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
-                image: "images/spaghetti.jpg",
-            },
-            {
-                title: "Pasta",
-                instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
-                ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
-                image: "images/spaghetti.jpg",
-            },
-            {
-                title: "Pasta",
-                instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
-                ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
-                image: "images/spaghetti.jpg",
-            },
-            {
-                title: "Pasta",
-                instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
-                ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
-                image: "images/spaghetti.jpg",
-            },
-            {
-                title: "Pasta",
-                instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
-                ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
-                image: "images/spaghetti.jpg",
-            },
-            {
-                title: "Pasta",
-                instructions: "Open Pasta box and boil for 16 minutes, until cooked and serve with sauce",
-                ingredients: ["Pasta", "3 cups of water", "Half cup of oil"],
-                image: "images/spaghetti.jpg",
-            },
-        ]
-    }
     static propTypes = {
         recipes: PropTypes.arrayOf(PropTypes.object).isRequired
     }
     render() {
         const recipes = this.props.recipes
-            .map((recipe, index) => (
-                <Recipe key={index} {...recipe} /> //...recipe is spread operator to include all the props
+            .map((recipe) => (
+                <Recipe key={recipe.id} {...recipe} /> //...recipe is spread operator to include all the props
             ));
         return (
             <div className="recipe-list">{recipes}</div>
